92a17033aae0c1baeb9d64f7fcf96f4b29a5d164
chengdol <chengdol@cloudsimple.com>
2022-12-18 12:26:18 -0800

    remove obsolote file

--- envoy/http-forward-proxy/run_envoy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-## run envoy config yaml file as specified in environment variable
-/usr/local/bin/envoy -c "/etc/envoy/config/${ENVOY_CONFIG_YAML}" --service-cluster front-proxy --log-level trace
